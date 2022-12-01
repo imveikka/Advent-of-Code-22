4d70f82caf8aeb588281ac8cb9f198fb4177d316
Veikka <imveikka@gmail.com>
2022-12-01 17:41:44 +0200

    Documentation to day 1 challenges

--- Day_1/E1.c
+++ Day_1/E1.c
@@ -1,7 +1,7 @@
-/*
-Advent of Code: Day 1, Problem 1
-Author: Veikka Immonen
-*/
+/*  Advent of Code: Day 1, Problem 1:
+    Find the elf with most calories carried.
+
+    Author: Veikka Immonen */
 
 
 #include <stdio.h>
@@ -11,29 +11,34 @@ Author: Veikka Immonen
 #define MAX(a, b) (a) > (b) ? (a) : (b)
 
 
+/*  All code is in main. The file name is given as
+    command line argument. Has simle error handling.
+    All output is printed to stdout. */
 int main(int argc, char *argv[]) {
 
     if (argc < 2) {
-        perror("Give filename\n");
-        exit(1);
+        printf("Give filename\n");
+        return 0;
     }
 
+    /* Variables to getline */
     char * line = NULL;
     size_t bytes = 0;
     size_t read; 
 
     FILE * file;
     if ((file = fopen(argv[1], "r")) == NULL) {
-        perror("File not found\n");
-        exit(1);
+        printf("File not found\n");
+        return 0;
     }
 
     int max = 0;
     int calories = 0;
 
-
+    /* getline returns -1 on failure/EOF */
     while ((read = getline(&line, &bytes, file)) != -1) {
         if (read == 1) {
+            /* line contains '\n' only */
             max = MAX(max, calories);
             calories = 0;
         }
@@ -41,6 +46,10 @@ int main(int argc, char *argv[]) {
     }
 
     fclose(file);
+
+    /*  After getline failure line is freed and set to NULL
+        which happens in this case. How ever it's good practice
+        to free memory manually just in case. */
     free(line);
 
     printf("Max elf is carrying %d calories.\n", max);
